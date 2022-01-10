eb102f596207528e57806af7fc845798807943d0
Patrícia Tersariol <patriciatersariol@gmail.com>
2022-01-09 23:20:42 -0300

    primeiro commit - projeto criado

---
+++ src/test/java/com/patriciatersariol/workshopspring/WorkshopspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patriciatersariol.workshopspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
