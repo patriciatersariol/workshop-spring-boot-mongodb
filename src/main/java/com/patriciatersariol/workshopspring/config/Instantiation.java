26be751c4af265be2eb50baf910aee01cd02d542
Patrícia Tersariol <patriciatersariol@gmail.com>
2022-01-11 21:27:41 -0300

    criando entidade Post

--- src/main/java/com/patriciatersariol/workshopspring/config/Instantiation.java
+++ src/main/java/com/patriciatersariol/workshopspring/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.patriciatersariol.workshopspring.config;
 
+import com.patriciatersariol.workshopspring.domain.Post;
 import com.patriciatersariol.workshopspring.domain.User;
+import com.patriciatersariol.workshopspring.repository.PostRepository;
 import com.patriciatersariol.workshopspring.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
@@ -14,14 +18,27 @@ public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
         userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Testando post 1", "Esse é o primeiro post", maria);
+        Post post2 = new Post(null, sdf.parse("21/03/2018"), "Testando post 2", "Esse é o segundo post", maria);
+        Post post3 = new Post(null, sdf.parse("21/03/2018"), "Testando post 3", "Esse é o segundo post", bob);
+
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
